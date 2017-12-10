6bb8ae97789bde3a89ec8dbe6005909735ebc1f1
swcooper2014 <swcooper2014@gmail.com>
2017-12-09 21:03:09 -0500

    message

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,6 @@
 
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 var wins = 0;
@@ -9,23 +10,21 @@ var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
 
-
-
-
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
 var updateGuessesLeft = function() {
-
+ 
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
 var updateLetterToGuess = function() {
   this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
+
 var updateGuessesSoFar = function() {
  
-  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+  document.querySelector('#guessedLetters').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
 var reset = function() {
@@ -36,11 +35,11 @@ var reset = function() {
   updateLetterToGuess();
   updateGuessesLeft();
   updateGuessesSoFar();
+
 }
 
 updateLetterToGuess();
 updateGuessesLeft();
-
 
 
 document.onkeyup = function(event) {
@@ -51,19 +50,18 @@ document.onkeyup = function(event) {
   updateGuessesLeft();
   updateGuessesSoFar();
 
-
-
-     if (guessesLeft > 0){
+        if (guessesLeft > 0){
             if (userGuess == letterToGuess){
-                wins++;
+                wins++
                 document.querySelector('#Wins').innerHTML = "Wins: " + wins;
                 alert("The letter to guess is " + letterToGuess);
                 reset();
             }
-        } else if(guessesLeft == 0){ 
-              losses++;
-              document.querySelector('#Losses').innerHTML = "Losses: " + losses;
-              alert("The letter to guess is " + letterToGuess);
-              reset();
+
+        }else if(guessesLeft == 0){ 
+            losses++;
+            document.querySelector('#Losses').innerHTML = "Losses: " + losses;
+            alert("The letter to guess is " + letterToGuess); 
+            reset();
         }
 };
